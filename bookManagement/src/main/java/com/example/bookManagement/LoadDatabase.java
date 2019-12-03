847433f996272f25a156b0201d749feede626e4c
pppt94 <pawel.pisarek@smcebi.edu.pl>
2019-12-03 22:14:03 +0100

    Add more data

--- bookManagement/src/main/java/com/example/bookManagement/LoadDatabase.java
+++ bookManagement/src/main/java/com/example/bookManagement/LoadDatabase.java
@@ -17,9 +17,25 @@ public class LoadDatabase {
     CommandLineRunner initDatabase(AuthorRepository authorRepository, BookRepository bookRepository) {
 
         return args -> {
-            Author aut = new Author("J.R.R. Tolkien");
-            authorRepository.save(aut);
-            bookRepository.save(new Book("Lord of The Rings", "fantasy", aut));
+
+            //Authors
+            Author aut1 = new Author("J.R.R. Tolkien");
+            authorRepository.save(aut1);
+            Author aut2 = new Author("George Orwell");
+            authorRepository.save(aut2);
+            Author aut3 = new Author("Joseph Heller");
+            authorRepository.save(aut3);
+            Author aut4 = new Author("Gabriel Garcia Marquez");
+            authorRepository.save(aut4);
+            Author aut5 = new Author("Andrzej Sapkowski");
+            authorRepository.save(aut5);
+
+            //Books
+            bookRepository.save(new Book("Lord of The Rings", "fantasy", aut1));
+            bookRepository.save(new Book("Hobbit", "fantasy", aut1));
+            bookRepository.save(new Book("Nineteen Eighty-Four", "novel", aut2));
+            bookRepository.save(new Book("Animal Farm", "novel", aut2));
+            bookRepository.save(new Book("Catch-22", "novel", aut3));
         };
     }
 }
