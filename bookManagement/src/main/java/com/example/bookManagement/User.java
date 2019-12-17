cf4e6fa5e743fda1466f94c2dbcf9b732d5b20ba
pppt94 <pawel.pisarek@smcebi.edu.pl>
2019-12-17 14:04:32 +0100

    Add unit test to UserService

--- bookManagement/src/main/java/com/example/bookManagement/User.java
+++ bookManagement/src/main/java/com/example/bookManagement/User.java
@@ -29,4 +29,8 @@ public class User {
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
+
+
+    public User() {
+    }
 }
---
+++ bookManagement/src/test/java/com/example/bookManagement/UserServiceTest.java
@@ -0,0 +1,64 @@
+package com.example.bookManagement;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+class UserServiceTest {
+
+    @Mock
+    private UserRepository mockUserRepository;
+    @Mock
+    private RoleRepository mockRoleRepository;
+    @Mock
+    private BCryptPasswordEncoder mockBCryptPasswordEncoder;
+
+    private UserService userServiceUnderTest;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+        userServiceUnderTest = new UserService(mockUserRepository,
+                mockRoleRepository,
+                mockBCryptPasswordEncoder);
+        user = new User();
+        user.setActive(1);
+        user.setLogin("TEST");
+        user.setEmail("test@test.com");
+
+
+        Mockito.when(mockUserRepository.save(any()))
+                .thenReturn(user);
+        Mockito.when(mockUserRepository.findByEmail(anyString()))
+                .thenReturn(user);
+    }
+
+    @Test
+    void findUserByEmail() {
+
+        final String email = "test@test.com";
+        final User result = userServiceUnderTest.findUserByEmail(email);
+
+        assertEquals(email, result.getEmail());
+    }
+
+    @Test
+    void saveUser() {
+
+        final String email = "test@test.com";
+        User result = userServiceUnderTest.saveUser(new User());
+
+        // Verify the results
+        assertEquals(email, result.getEmail());
+    }
+}
