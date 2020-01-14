c075154a84c9d31178f1ceca1efcca5902f1593b
pppt94 <pawel.pisarek@smcebi.edu.pl>
2020-01-14 15:07:36 +0100

    Add Role&Security files

--- bookManagement/src/main/java/com/example/bookManagement/Controller.java
+++ bookManagement/src/main/java/com/example/bookManagement/Controller.java
@@ -1,6 +1,8 @@
 package com.example.bookManagement;
 
 import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,5 +15,18 @@ public class Controller {
     public String view () {
         return "index.html";
     }
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
 
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/user")
+    public String userIndex() {
+        return "user/index";
+    }
 }
