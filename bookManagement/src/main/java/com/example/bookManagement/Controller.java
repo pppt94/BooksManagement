ec5fb448e81bc10bf2a7e7a89351610470d51c66
pppt94 <pawel.pisarek@smcebi.edu.pl>
2020-01-18 15:42:14 +0100

    Add logout

--- bookManagement/src/main/java/com/example/bookManagement/Controller.java
+++ bookManagement/src/main/java/com/example/bookManagement/Controller.java
@@ -1,10 +1,16 @@
 package com.example.bookManagement;
 
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -56,5 +62,14 @@ public class Controller {
     @GetMapping("/stats")
     public String stats() {
         return "stats";
+    }
+
+    @RequestMapping(value="/logout", method=RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
     }
 }
